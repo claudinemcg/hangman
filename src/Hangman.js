6d669ac6b1ec66b25c239daf136d8b7cc1b94b26
Claudine McGowan <40922123+claudinemcg@users.noreply.github.com>
2023-09-20 10:37:14 +0100

    add alt text

--- src/Hangman.js
+++ src/Hangman.js
@@ -64,7 +64,7 @@ class Hangman extends Component {
     return (
       <div className='Hangman'>
         <h1>Hangman</h1>
-        <img src={this.props.images[this.state.nWrong]} />
+        <img src={this.props.images[this.state.nWrong]} alt={`${this.state.nWrong} out of ${this.props.maxWrong}`}/>
         <p className='Hangman-score'> {`Guessed wrong: ${this.state.nWrong}`}</p>
         <p className='Hangman-word'>
           {gameNotOver 
